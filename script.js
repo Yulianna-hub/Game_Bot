ff789655671068e81a01130e5d5a2386eb701eff
Yuliia Borys <yuliannaboris@gmail.com>
2020-04-06 20:26:36 +0200

    script.js

--- script.js
+++ script.js
@@ -52,27 +52,4 @@ if (isNumber(x)) {
 }
 guessedNumber();
 
-/*if (isNumber(x)) {
-    if (number < x) {
-        alert('Загаданное число меньше. Введи новое число!');
-        checkNumber();
-    } else if (number > x) {
-        alert('Загаданное число больше. Введи новый вариант!');
-        checkNumber();
-    } else if (number == x) {
-        alert('Поздравляю! Вы угадали!');
-        let convey = confirm('Хотите сыграть еще?');
-        if (convey == true) {
-            guessedNumber();
-        } else if (convey == false) {
-            alert('Игра окончена!');
-        }
-
-    }
-} else if (x === null) {
-    alert('Игра окончена!');
-} else {
-    alert('Введи число!');
-    checkNumber();
-}*/
 
